34e2e517df7aa5a7a4ff94e79a8f52052e879a89
CypherSignal <davidacrooks@outlook.com>
2016-06-26 13:22:51 -0400

    Implementation of indexed Get function

--- include/EASTL/soavector.h
+++ include/EASTL/soavector.h
@@ -12,11 +12,60 @@ namespace eastl
 {
 // non-recursive soa_vector implementation
 
+template <typename... Ts>
+class soa_vector;
+
+template <size_t I, typename SoaVector>
+class soavec_element;
+
+template <size_t I, typename SoaVector>
+using soavec_element_t = typename soavec_element<I, SoaVector>::type;
+
 namespace Internal
 {
+	template <typename Indices, typename... Ts>
+	struct SoaVecImpl;
+}
 
-template <typename SoaVecIndices, typename... Ts>
-struct SoaVecImpl;
+// soavec_element helper to be able to isolate a type given an index
+template <typename... Ts>
+struct SoaVecTypes
+{
+};
+
+template <size_t I, typename T>
+class soavec_element
+{
+};
+
+template <size_t I>
+class soavec_element<I, soa_vector<>> // todo tuple implementation doesn't need this to be the literal soa_vector type, and is able to use "SoaVecTypes"
+{
+public:
+	static_assert(I != I, "tuple_element index out of range");
+};
+
+template <typename H, typename... Ts>
+class soavec_element<0, soa_vector<H, Ts...>>
+{
+public:
+	typedef H type;
+};
+
+template <size_t I, typename H, typename... Ts>
+class soavec_element<I, soa_vector<H, Ts...>>
+{
+public:
+	typedef soavec_element_t<I - 1, soa_vector<Ts...>> type;
+};
+
+template <size_t I, typename Indices, typename... Ts>
+class soavec_element<I, Internal::SoaVecImpl<Indices, Ts...>> : public soavec_element<I, soa_vector<Ts...>>
+{
+};
+
+namespace Internal
+{
 
 // SoaVecLeaf
 template <size_t I, typename ValueType>
@@ -25,16 +74,21 @@ class SoaVecLeaf
 public:
 	SoaVecLeaf() : mVec() {}
 
+	// swallow function requires non-void return types
 	void* push_back()
 	{
 		mVec.push_back();
 		return nullptr;
 	}
+
+	vector<ValueType>& getInternal() { return mVec; }
 private:
 	vector<ValueType> mVec;
 };
 
 // SoaVecImpl
+
+// swallow allows for parameter pack expansion of arguments as means of expanding operations performed
 template <typename... Ts>
 void swallow(Ts&&...)
 {
@@ -53,6 +107,12 @@ public:
 	}
 };
 
+template <size_t I, typename Indices, typename... Ts>
+vector<soavec_element_t<I, SoaVecImpl<Indices, Ts...>>>& get(SoaVecImpl<Indices, Ts...>& t)
+{
+	typedef soavec_element_t<I, SoaVecImpl<Indices, Ts...>> Type;
+	return static_cast<Internal::SoaVecLeaf<I, Type>&>(t).getInternal();
+}
 
 }  // namespace Internal
 
@@ -65,6 +125,12 @@ public:
 	EA_CONSTEXPR soa_vector() = default;
 
 	void push_back() { mImpl.push_back(); }
+	
+	template<size_t I>
+	vector<soavec_element_t<I, soa_vector<Ts...>>>& get()
+	{
+		return Internal::get<I>(mImpl);
+	}
 
 private:
 	typedef Internal::SoaVecImpl<make_index_sequence<sizeof...(Ts)>, Ts...> Impl;
@@ -74,4 +140,4 @@ private:
 
 }  // namespace eastl
 
-#endif  // EASTL_TUPLE_H
+#endif  // EASTL_SOAVECTOR_H
--- test/source/TestSoaVector.cpp
+++ test/source/TestSoaVector.cpp
@@ -8,26 +8,43 @@
 
 #include <EASTL/soavector.h>
 
-//EASTL_SOA_VECTOR_DECL(IntSoaVec
-//	int, element
-//)
+using namespace eastl;
 
 int TestSoaVector()
 {
-	using namespace eastl;
-
 	int nErrorCount = 0;
 
 	// Test empty push-backs
 	{
 		soa_vector<int> singleElementVec;
 		singleElementVec.push_back();
-//		EATEST_VERIFY(get<0>(singleElementVec).size()  == 1);
+		vector<int>& vec = (singleElementVec).get<0>();
+		EATEST_VERIFY(vec.size() == 1);
+		EATEST_VERIFY(singleElementVec.get<0>().size() == 1);
 
 		soa_vector<int, float> doubleElementVec;
 		doubleElementVec.push_back();
-		//EATEST_VERIFY(get<0>(doubleElementVec).size() == 1);
-		//EATEST_VERIFY(get<1>(doubleElementVec).size() == 1);
+		EATEST_VERIFY(doubleElementVec.get<0>().size() == 1);
+		EATEST_VERIFY(doubleElementVec.get<1>().size() == 1);
+		EATEST_VERIFY((void*)doubleElementVec.get<0>().data() !=
+			(void*)doubleElementVec.get<1>().data());
+
+		typedef struct
+		{
+			float a;
+			int b;
+			float c;
+			float d[4];
+		} NonprimitiveStruct;
+
+		soa_vector<int, NonprimitiveStruct> nonPrimitveElementVec;
+		vector<NonprimitiveStruct>& npsVec = nonPrimitveElementVec.get<1>();
+		nonPrimitveElementVec.push_back();
+		nonPrimitveElementVec.get<0>()[0] = 0;
+		nonPrimitveElementVec.get<1>()[0].a = 0.0f;
+		nonPrimitveElementVec.get<1>()[0].b = 1;
+
+		EATEST_VERIFY(npsVec[0].b == 1);
 
 		//IntSoaVec macroSingleElementVec;
 		//macroSingleElementVec.push_back(1);
