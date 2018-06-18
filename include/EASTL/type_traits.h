899fc72c32f2cf54074aeda77a35fe2644891dc4
Andrey Zheleznov <zheland.net@gmail.com>
2018-06-18 13:10:20 +0300

    Fix constexpr specifier for integral_constant
    integral_constant<T, v>::value should be constexpr.
    
    This fix use of constexpr non-integral constants, e.g.:
    type_traits.h:264:18: error: `constexpr` needed
    for in-class initialization of static data member
    `int* const eastl::integral_constant<int*, 0u>::value`
    of non-integral type [-fpermissive]

--- include/EASTL/type_traits.h
+++ include/EASTL/type_traits.h
@@ -261,7 +261,7 @@ namespace eastl
 	template <typename T, T v>
 	struct integral_constant
 	{
-		static const T value = v;
+		static EA_CONSTEXPR T value = v;
 		typedef T value_type;
 		typedef integral_constant<T, v> type;
 
