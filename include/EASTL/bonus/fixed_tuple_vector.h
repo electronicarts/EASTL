cb5be931c1ad4b04b268b466c1653978d578757a
David Crooks <davidacrooks@outlook.com>
2018-06-24 19:19:26 -0400

    Initial update of FixedTupleVector test (and support for them)

--- include/EASTL/bonus/fixed_tuple_vector.h
+++ include/EASTL/bonus/fixed_tuple_vector.h
@@ -24,15 +24,31 @@ private:
 		TupleVecInternal::TupleRecurser<Ts...>::GetTotalAlignment(), 0,
 		bEnableOverflow, EASTLAllocatorType> fixed_allocator_type;
 
-	typedef TupleVecInternal::TupleVecImpl<fixed_allocator_type, make_index_sequence<sizeof...(Ts)>, Ts...> Impl;
+	typedef TupleVecInternal::TupleVecImpl<fixed_allocator_type, make_index_sequence<sizeof...(Ts)>, Ts...> base_type;
 	typedef aligned_buffer<fixed_allocator_type::kNodesSize, fixed_allocator_type::kNodeAlignment> aligned_buffer_type;
 
 	aligned_buffer_type mBuffer;
 
+	using base_type::base_type;
+
+	typedef typename base_type::size_type size_type;
+
 public:
 	fixed_tuple_vector()
-		:Impl(fixed_allocator_type(mBuffer.buffer), mBuffer.buffer, nodeCount)
+		:base_type(fixed_allocator_type(mBuffer.buffer), mBuffer.buffer, nodeCount)
 	{ }
+
+	// Returns the max fixed size, which is the user-supplied nodeCount parameter.
+	size_type max_size() const { return nodeCount; }
+	// Returns true if the fixed space has been fully allocated. Note that if overflow is enabled,
+	// the container size can be greater than nodeCount but full() could return true because the
+	// fixed space may have a recently freed slot.
+	bool full() const { return (mNumElements >= nodeCount) || ((void*)mpData != (void*)mBuffer.buffer);	}
+	// Returns true if the allocations spilled over into the overflow allocator. Meaningful
+	// only if overflow is enabled.
+	bool has_overflowed() const { return ((void*)mpData != (void*)mBuffer.buffer); }
+	// Returns the value of the bEnableOverflow template parameter.
+	bool can_overflow() const { return bEnableOverflow; }
 };
 
 }  // namespace eastl
--- test/source/TestFixedTupleVector.cpp
+++ test/source/TestFixedTupleVector.cpp
@@ -12,134 +12,21 @@
 #include <EASTL/sort.h>
 #include <EASTL/random.h>
 
+
+#include "EASTL/fixed_vector.h"
+
 using namespace eastl;
 
 template <size_t nodeCount, bool bEnableOverflow>
-int TestTupleVectorIterator()
-{
-	int nErrorCount = 0;
-
-	fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int> tripleElementVec;
-	tripleElementVec.push_back(1, 2.0f, 6);
-	tripleElementVec.push_back(2, 3.0f, 7);
-	tripleElementVec.push_back(3, 4.0f, 8);
-	tripleElementVec.push_back(4, 5.0f, 9);
-	tripleElementVec.push_back(5, 6.0f, 10);
-
-
-	// test copyConstructible, copyAssignable, swappable, prefix inc, !=, reference convertible to value_type (InputIterator!)
-	{
-		fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator iter = tripleElementVec.begin();
-		++iter;
-		auto copiedIter(iter);
-		EATEST_VERIFY(get<2>(*copiedIter) == 7);
-		EATEST_VERIFY(copiedIter == iter);
-
-		++iter;
-		copiedIter = iter;
-		EATEST_VERIFY(get<2>(*copiedIter) == 8);
-
-		++iter;
-		swap(iter, copiedIter);
-		EATEST_VERIFY(get<2>(*iter) == 8);
-		EATEST_VERIFY(get<2>(*copiedIter) == 9);
-
-		EATEST_VERIFY(copiedIter != iter);
-
-		tuple<int&, float&, int&> ref(*iter);
-		tuple<int, float, int> value(*iter);
-		EATEST_VERIFY(get<2>(ref) == get<2>(value));
-	}
-
-	// test postfix increment, default constructible (ForwardIterator)
-	{
-		fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator iter = tripleElementVec.begin();
-		auto prefixIter = ++iter;
-
-		fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator postfixIter;
-		postfixIter = iter++;
-		EATEST_VERIFY(prefixIter == postfixIter);
-		EATEST_VERIFY(get<2>(*prefixIter) == 7);
-		EATEST_VERIFY(get<2>(*iter) == 8);
-	}
-
-	// test prefix decrement and postfix decrement (BidirectionalIterator)
-	{
-		fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator iter = tripleElementVec.end();
-		auto prefixIter = --iter;
-
-		fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator postfixIter;
-		postfixIter = iter--;
-		EATEST_VERIFY(prefixIter == postfixIter);
-		EATEST_VERIFY(get<2>(*prefixIter) == 10);
-		EATEST_VERIFY(get<2>(*iter) == 9);
-	}
-
-	// test many arithmetic operations (RandomAccessIterator)
-	{
-		fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator iter = tripleElementVec.begin();
-		auto symmetryOne = iter + 2;
-		auto symmetryTwo = 2 + iter;
-		iter += 2;
-		EATEST_VERIFY(symmetryOne == symmetryTwo);
-		EATEST_VERIFY(symmetryOne == iter);
-
-		symmetryOne = iter - 2;
-		symmetryTwo = 2 - iter;
-		iter -= 2;
-		EATEST_VERIFY(symmetryOne == symmetryTwo);
-		EATEST_VERIFY(symmetryOne == iter);
-
-		iter += 2;
-		EATEST_VERIFY(iter - symmetryOne == 2);
-
-		tuple<int&, float&, int&> symmetryRef = symmetryOne[2];
-		EATEST_VERIFY(get<2>(symmetryRef) == get<2>(*iter));
-
-		EATEST_VERIFY(symmetryOne < iter);
-		EATEST_VERIFY(iter > symmetryOne);
-		EATEST_VERIFY(symmetryOne >= symmetryTwo && iter >= symmetryOne);
-		EATEST_VERIFY(symmetryOne <= symmetryTwo && symmetryOne <= iter);
-	}
-
-	{
-		float i = 0;
-		int j = 0;
-		EATEST_VERIFY(&get<0>(*tripleElementVec.begin()) == tripleElementVec.get<0>());
-		EATEST_VERIFY(&get<1>(*tripleElementVec.begin()) == tripleElementVec.get<1>());
-		for (auto& iter : tripleElementVec)
-		{
-			i += get<1>(iter);
-			j += get<2>(iter);
-		}
-		EATEST_VERIFY(i == 20.0f);
-		EATEST_VERIFY(j == 40);
-
-		float reverse_i = 0;
-		int reverse_j = 0;
-
-		eastl::for_each(tripleElementVec.rbegin(), tripleElementVec.rend(),
-			[&](const tuple<int, float, int> tup)
-		{
-			reverse_i += get<1>(tup);
-			reverse_j += get<2>(tup);
-		});
-		EATEST_VERIFY(i == reverse_i);
-		EATEST_VERIFY(j == reverse_j);
-		EATEST_VERIFY(get<0>(*tripleElementVec.rbegin()) == 5);
-	}
-	return nErrorCount;
-}
-
-int TestFixedTupleVector()
+int TestFixedTupleVectorVariant()
 {
 	int nErrorCount = 0;
 
 	// Test uninit'ed push-backs
 	{
-		fixed_tuple_vector<4, false, int> singleElementVec;
+		fixed_tuple_vector<nodeCount, bEnableOverflow, int> singleElementVec;
 		EATEST_VERIFY(singleElementVec.size() == 0);
-		EATEST_VERIFY(singleElementVec.capacity() == 4);
+		EATEST_VERIFY(singleElementVec.capacity() == nodeCount);
 		EATEST_VERIFY(singleElementVec.empty() == true);
 		singleElementVec.push_back_uninitialized();
 		singleElementVec.push_back(5);
@@ -149,15 +36,9 @@ int TestFixedTupleVector()
 		EATEST_VERIFY(singleElementVec.empty() == false);
 
 
-		fixed_tuple_vector<4, true, int, float, bool> complexVec;
+		fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, bool> complexVec;
 		complexVec.push_back(3, 2.0f, true);
 		complexVec.push_back(1, 4.0f, false);
-		complexVec.push_back(2, 1.0f, true);
-		complexVec.push_back(4, 3.0f, false);
-		EATEST_VERIFY(*(complexVec.get<0>()) == 3);
-		EATEST_VERIFY(complexVec.get<float>()[1] == 4.0f);
-		EATEST_VERIFY(complexVec.get<2>()[2] == complexVec.get<bool>()[2]);
-
 		complexVec.push_back(2, 1.0f, true);
 		complexVec.push_back(4, 3.0f, false);
 		EATEST_VERIFY(*(complexVec.get<0>()) == 3);
@@ -174,25 +55,44 @@ int TestFixedTupleVector()
 		EATEST_VERIFY(get<1>(complexVec.front()) == 2.0f);
 		EATEST_VERIFY(get<1>(complexVec.back()) == 3.0f);
 
+		// verify the equivalent accessors for the const container exist/compile
+		{
+			const fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, bool>& constVec = complexVec;
+			EATEST_VERIFY(constVec.size() == 4);
+			EATEST_VERIFY(constVec.capacity() >= constVec.size());
+			EATEST_VERIFY(constVec.empty() == false);
+			EATEST_VERIFY(constVec.get<1>() == constVec.get<float>());
+
+			tuple<const int*, const float*, const bool*> constPtrTuple = constVec.data();
+			EATEST_VERIFY(get<0>(constPtrTuple) != nullptr);
+			EATEST_VERIFY(get<2>(constPtrTuple)[2] == constVec.get<2>()[2]);
+
+			tuple<const int&, const float&, const bool&> constRefTuple = constVec.at(2);
+			EATEST_VERIFY(get<2>(constRefTuple) == constVec.get<2>()[2]);
+			EATEST_VERIFY(get<1>(constVec[2]) == 1.0f);
+			EATEST_VERIFY(get<1>(constVec.front()) == 2.0f);
+			EATEST_VERIFY(get<1>(constVec.back()) == 3.0f);
+		}
+
 		__declspec(align(16)) struct AlignTestVec4
 		{
 			float a[4];
-			AlignTestVec4() :a{ 1.0f, 2.0f, 3.0f, 4.0f } {}
+			AlignTestVec4() : a{1.0f, 2.0f, 3.0f, 4.0f} {}
 		};
 
 		struct AlignTestByte3
 		{
 			char a[3];
-			AlignTestByte3() :a{ 1, 2, 3 } {}
+			AlignTestByte3() : a{1, 2, 3} {}
 		};
 
 		__declspec(align(8)) struct AlignTestFourByte
 		{
 			int a[5];
-			AlignTestFourByte() :a{ -1, -2, -3, -4, -5 } {}
+			AlignTestFourByte() : a{-1, -2, -3, -4, -5} {}
 		};
 
-		fixed_tuple_vector<16, false, bool, AlignTestVec4, AlignTestByte3, AlignTestFourByte> alignElementVec;
+		fixed_tuple_vector<nodeCount, bEnableOverflow, bool, AlignTestVec4, AlignTestByte3, AlignTestFourByte> alignElementVec;
 		alignElementVec.push_back();
 		alignElementVec.push_back();
 		alignElementVec.push_back();
@@ -203,7 +103,7 @@ int TestFixedTupleVector()
 	{
 		TestObject::Reset();
 
-		fixed_tuple_vector<20, true, bool, TestObject, float> testVec;
+		fixed_tuple_vector<nodeCount, bEnableOverflow, bool, TestObject, float> testVec;
 		for (int i = 0; i < 10; ++i)
 		{
 			testVec.push_back(i % 3 == 0, TestObject(i), (float)i);
@@ -217,8 +117,12 @@ int TestFixedTupleVector()
 		testVec.push_back();
 		testVec.pop_back();
 		EATEST_VERIFY(testVec.capacity() != 10);
-		testVec.shrink_to_fit();
-		EATEST_VERIFY(testVec.capacity() == 10);
+
+		if (testVec.can_overflow())
+		{
+			testVec.shrink_to_fit();
+			EATEST_VERIFY(testVec.capacity() == 10);
+		}
 
 		testVec.clear();
 		EATEST_VERIFY(testVec.empty());
@@ -228,9 +132,127 @@ int TestFixedTupleVector()
 	}
 
 	// Test fixed_tuple_Vector in a ranged for, and other large-scale iterator testing
-	nErrorCount += TestTupleVectorIterator<4, true>();
-	nErrorCount += TestTupleVectorIterator<16, false>();
-	nErrorCount += TestTupleVectorIterator<16, true>();
+	{
+		fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int> tripleElementVec;
+		tripleElementVec.push_back(1, 2.0f, 6);
+		tripleElementVec.push_back(2, 3.0f, 7);
+		tripleElementVec.push_back(3, 4.0f, 8);
+		tripleElementVec.push_back(4, 5.0f, 9);
+		tripleElementVec.push_back(5, 6.0f, 10);
+
+		// test copyConstructible, copyAssignable, swappable, prefix inc, !=, reference convertible to value_type (InputIterator!)
+		{
+			fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator iter = tripleElementVec.begin();
+			++iter;
+			auto copiedIter(iter);
+			EATEST_VERIFY(get<2>(*copiedIter) == 7);
+			EATEST_VERIFY(copiedIter == iter);
+
+			++iter;
+			copiedIter = iter;
+			EATEST_VERIFY(get<2>(*copiedIter) == 8);
+
+			++iter;
+			swap(iter, copiedIter);
+			EATEST_VERIFY(get<2>(*iter) == 8);
+			EATEST_VERIFY(get<2>(*copiedIter) == 9);
+
+			EATEST_VERIFY(copiedIter != iter);
+
+			tuple<int&, float&, int&> ref(*iter);
+			tuple<int, float, int> value(*iter);
+			EATEST_VERIFY(get<2>(ref) == get<2>(value));
+		}
+
+		// test postfix increment, default constructible (ForwardIterator)
+		{
+			fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator iter = tripleElementVec.begin();
+			auto prefixIter = ++iter;
+
+			fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator postfixIter;
+			postfixIter = iter++;
+			EATEST_VERIFY(prefixIter == postfixIter);
+			EATEST_VERIFY(get<2>(*prefixIter) == 7);
+			EATEST_VERIFY(get<2>(*iter) == 8);
+		}
+
+		// test prefix decrement and postfix decrement (BidirectionalIterator)
+		{
+			fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator iter = tripleElementVec.end();
+			auto prefixIter = --iter;
+
+			fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator postfixIter;
+			postfixIter = iter--;
+			EATEST_VERIFY(prefixIter == postfixIter);
+			EATEST_VERIFY(get<2>(*prefixIter) == 10);
+			EATEST_VERIFY(get<2>(*iter) == 9);
+		}
+
+		// test many arithmetic operations (RandomAccessIterator)
+		{
+			fixed_tuple_vector<nodeCount, bEnableOverflow, int, float, int>::iterator iter = tripleElementVec.begin();
+			auto symmetryOne = iter + 2;
+			auto symmetryTwo = 2 + iter;
+			iter += 2;
+			EATEST_VERIFY(symmetryOne == symmetryTwo);
+			EATEST_VERIFY(symmetryOne == iter);
+
+			symmetryOne = iter - 2;
+			symmetryTwo = 2 - iter;
+			iter -= 2;
+			EATEST_VERIFY(symmetryOne == symmetryTwo);
+			EATEST_VERIFY(symmetryOne == iter);
+
+			iter += 2;
+			EATEST_VERIFY(iter - symmetryOne == 2);
+
+			tuple<int&, float&, int&> symmetryRef = symmetryOne[2];
+			EATEST_VERIFY(get<2>(symmetryRef) == get<2>(*iter));
+
+			EATEST_VERIFY(symmetryOne < iter);
+			EATEST_VERIFY(iter > symmetryOne);
+			EATEST_VERIFY(symmetryOne >= symmetryTwo && iter >= symmetryOne);
+			EATEST_VERIFY(symmetryOne <= symmetryTwo && symmetryOne <= iter);
+		}
+
+		{
+			float i = 0;
+			int j = 0;
+			EATEST_VERIFY(&get<0>(*tripleElementVec.begin()) == tripleElementVec.get<0>());
+			EATEST_VERIFY(&get<1>(*tripleElementVec.begin()) == tripleElementVec.get<1>());
+			for (auto& iter : tripleElementVec)
+			{
+				i += get<1>(iter);
+				j += get<2>(iter);
+			}
+			EATEST_VERIFY(i == 20.0f);
+			EATEST_VERIFY(j == 40);
+
+			float reverse_i = 0;
+			int reverse_j = 0;
+
+			eastl::for_each(tripleElementVec.rbegin(), tripleElementVec.rend(),
+				[&](const tuple<int, float, int> tup)
+			{
+				reverse_i += get<1>(tup);
+				reverse_j += get<2>(tup);
+			});
+			EATEST_VERIFY(i == reverse_i);
+			EATEST_VERIFY(j == reverse_j);
+			EATEST_VERIFY(get<0>(*tripleElementVec.rbegin()) == 5);
+		}
+	}
+
+	return nErrorCount;
+}
+
+int TestFixedTupleVector()
+{
+	int nErrorCount = 0;
+
+	nErrorCount += TestFixedTupleVectorVariant<4, true>();
+	nErrorCount += TestFixedTupleVectorVariant<16, true>();
+	nErrorCount += TestFixedTupleVectorVariant<64, false>();
 
 	// test sort.h
 	if(0)
