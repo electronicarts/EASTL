2f45c3a8f670d7d6afcc5f106418f4c51f2107db
David Crooks <davidacrooks@outlook.com>
2018-07-04 11:44:06 -0400

    Add pragma once to fixed_tuple_Vector.h

--- include/EASTL/bonus/fixed_tuple_vector.h
+++ include/EASTL/bonus/fixed_tuple_vector.h
@@ -8,6 +8,10 @@
 #include <EASTL/bonus/tuple_vector.h>
 #include <EASTL/internal/fixed_pool.h>
 
+#if defined(EA_PRAGMA_ONCE_SUPPORTED)
+	#pragma once // Some compilers (e.g. VC++) benefit significantly from using this. We've measured 3-4% build speed improvements in apps as a result.
+#endif
+
 namespace eastl
 {
 
