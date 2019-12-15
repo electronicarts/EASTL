a8fb05b56e72b035219504329ea6cc8574f991ad
Rob Parolin <robertoparolin@gmail.com>
2019-12-14 16:51:07 -0800

    Ensuring builds succeed when tests and benchmarks are toggled

--- build.sh
+++ build.sh
@@ -1,15 +1,27 @@
 build_folder=build
+
+rm -rf $build_folder
 mkdir $build_folder
 pushd $build_folder
+
+cmake .. -DEASTL_BUILD_TESTS:BOOL=OFF -DEASTL_BUILD_BENCHMARK:BOOL=ON
+cmake --build . --config Release -- -j 32 
+
+cmake .. -DEASTL_BUILD_TESTS:BOOL=OFF -DEASTL_BUILD_BENCHMARK:BOOL=OFF
+cmake --build . --config Release -- -j 32 
+ 
 cmake .. -DEASTL_BUILD_TESTS:BOOL=ON -DEASTL_BUILD_BENCHMARK:BOOL=OFF
-cmake --build . --config Release
-# cmake --build . --config Debug
-# cmake --build . --config RelWithDebInfo
-# cmake --build . --config MinSizeRel
+cmake --build . --config Release -- -j 32 
+ 
+cmake .. -DEASTL_BUILD_TESTS:BOOL=ON -DEASTL_BUILD_BENCHMARK:BOOL=ON
+cmake --build . --config Release -- -j 32 
+cmake --build . --config Debug -- -j 32
+cmake --build . --config RelWithDebInfo -- -j 32
+cmake --build . --config MinSizeRel -- -j 32
 pushd test
 ctest -C Release -V
-# ctest -C Debug
-# ctest -C RelWithDebInfo
-# ctest -C MinSizeRel
+ctest -C Debug -V
+ctest -C RelWithDebInfo -V
+ctest -C MinSizeRel -V
 popd
 popd
