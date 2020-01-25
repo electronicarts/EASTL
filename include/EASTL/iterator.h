f98a30c1dcce64180b05c40adfd43249aad2fcc0
Daniel Yates <me@meorawr.io>
2020-01-25 19:43:52 +0000

    Fix GCC 9.2 constexpr compile errors in array tests (#344)
    For some reason GCC 9.2 chokes on the subscript operator of
    reverse_iterator. Using the (commented out) `-n - 1` expression
    instead seems to fix this.

--- include/EASTL/iterator.h
+++ include/EASTL/iterator.h
@@ -311,11 +311,8 @@ namespace eastl
 		// random_access_iterator operator[] is merely required to return something convertible to reference. 
 		// reverse_iterator operator[] can't necessarily know what to return as the underlying iterator 
 		// operator[] may return something other than reference.
-		// reference operator[](difference_type n) const
-		//     { return mIterator[-n - 1]; }
-
 		EA_CPP14_CONSTEXPR reference operator[](difference_type n) const
-			{ return *(*this + n); }
+			{ return mIterator[-n - 1]; }
 	};
 
 
